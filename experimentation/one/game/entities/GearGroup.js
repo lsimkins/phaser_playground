35f4d0aa64169cc63fbf7653032c806b449b280a
Luke Simkins <lucas.simkins@gmail.com>
2014-07-12 15:36:01 -0500

    Gears now rotate only their connectect neighbors.

--- experimentation/one/game/entities/GearGroup.js
+++ experimentation/one/game/entities/GearGroup.js
@@ -59,18 +59,22 @@ VG.GearGroup.prototype.rotateGearAt = function(x, y, dir, rotateNeighbors, force
   rotateNeighbors = (typeof rotateNeighbors === undefined) ? rotateNeighbors : true;
   force           = (typeof force === undefined) ? force : false;
 
+  var neighbors;
   var gear = this.grid[x] ? this.grid[x][y] : undefined;
   if (!gear || (gear.rotatedThisTurn && !force)) {
     return;
   }
 
+  if (rotateNeighbors) {
+    neighbors = this.getConnectedNeighbors(x, y);
+  }
+
   gear.rotate(dir);
-  gear.rotatedThisTurn = true;
+  // gear.rotatedThisTurn = true;
 
   if (rotateNeighbors) {
-    var neighbors = this.getNeighbors(x, y);
     for (var i = neighbors.length - 1; i >= 0; i--) {
-      this.rotateGearAt(neighbors[i][0], neighbors[i][1]);
+      this.rotateGearAt(neighbors[i][0], neighbors[i][1], dir * -1);
     }
   }
 
@@ -106,6 +110,37 @@ VG.GearGroup.prototype.getNeighbors = function(x, y) {
 
   if (this.grid[y-1] && this.grid[y-1][y]) {
     neighbors.push([x, y-1]);
+  }
+
+  return neighbors;
+};
+
+VG.GearGroup.prototype.getConnectedNeighbors = function(x, y) {
+  var neighbors = [];
+    var gear = this.grid[x][y];
+
+  if (gear.sides[1] && this.grid[x+1] && this.grid[x+1][y]) {
+    if (this.grid[x+1][y].sides[3]) {
+      neighbors.push([x+1, y]);
+    }
+  }
+
+  if (gear.sides[3] && this.grid[x-1] && this.grid[x-1][y]) {
+    if (this.grid[x-1][y].sides[1]) {
+      neighbors.push([x-1, y]);
+    }
+  }
+
+  if (gear.sides[2] && this.grid[x][y+1] && this.grid[x][y+1]) {
+    if (this.grid[x][y+1].sides[0]) {
+      neighbors.push([x, y+1]);
+    }
+  }
+
+  if (gear.sides[0] && this.grid[x][y-1] && this.grid[x][y-1]) {
+    if (this.grid[x][y-1].sides[2]) {
+      neighbors.push([x, y-1]);
+    }
   }
 
   return neighbors;
